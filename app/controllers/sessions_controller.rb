1273f94a326b61624cdd253d8d658ce4e07fc479
josephpgutierrez <josephpgutierrez@yahoo.com>
2009-07-30 14:58:49 -0700

    Continuing to add specs.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
       redirect_back_or_default(url_for(:controller => :users, :action => :show, :id => self.current_user.id))
-      flash.now[:notice] = "Logged in successfully"
+      flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
       @login       = params[:login]
