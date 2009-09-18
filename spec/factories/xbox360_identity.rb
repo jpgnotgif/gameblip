78c19670aab23a3b538551fe5a616ad31719482a
josephpgutierrez <josephpgutierrez@yahoo.com>
2009-09-18 16:46:17 -0700

    Removing renamed files

--- spec/factories/xbox360_identity.rb
+++
@@ -1,8 +0,0 @@
-Factory.define :xbox360_identity do |f|    
-  xml_file_data = File.open(File.join(RAILS_ROOT, "spec/files/xml/xbox_live/jpgnotgif.xml"), "r") { |file| file.read }
-  hash_data = HashExtras.underscorize_and_symbolize_all_keys!(Hash.from_xml(xml_file_data))
-  f.account_status hash_data[:xbox_info][:account_status]
-  f.gamerscore hash_data[:xbox_info][:gamer_score]
-  f.reputation hash_data[:xbox_info][:reputation]
-  f.motto "Do work!"
-end
