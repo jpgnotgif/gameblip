ca1082c8dc1d11ac21737ae92a18cff7d13b0c28
josephpgutierrez <josephpgutierrez@yahoo.com>
2009-09-28 17:23:44 -0700

    Finished activity spec changes

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -5,58 +5,55 @@ describe Activity do
     @xml = File.open(File.join(RAILS_ROOT, "spec/files/xml/xbox_live", "jpgnotgif.xml"), "r") { |f| f.read }
     @url = AppConfig.instance_variable_get(:@config).xbox_api.url
     @user = users(:josephpgutierrez)
-    @xbox_console_user = Factory.build(:xbox_console_user, {:user => @user})
-    Net::HTTP.expects(:get).with(URI.parse(@url + @xbox_console_user.gamertag)).returns(@xml)
-    @xbox_console_user.save
-    @activity = Factory.build(:activity, {:avatar_id => @xbox_console_user.id, :avatar_type => @xbox_console_user.class.name})
+    @gamer_identity_attributes = Factory.attributes_for(:gamer_identity).merge(:user_id => @user.id)
+    @xbox_identity = Factory.build(:xbox_identity, @gamer_identity_attributes)
+    Net::HTTP.expects(:get).with(URI.parse(@url + @xbox_identity.name)).returns(@xml)
+    @xbox_identity.save!
+    @activity = Factory.build(:activity, {:avatar_id => @xbox_identity.id, :avatar_type => @xbox_identity.type})
   end
 
   it "should save" do
-    lambda {
-      @activity.save.should be_true
-      @xbox_console_user.activities.count.should == 1
-    }.should change(Activity, :count).by(1)
+    @activity.save.should be_true
+    @xbox_identity.activities.should have(1).record
+    Activity.should have(1).record
   end
 
   it "requires non-blank description" do
-    lambda {
-      @activity.description = ""
-      @activity.save.should be_false
-      @activity.errors.on(:description).should be_true
-      @xbox_console_user.activities.count.should == 0
-    }.should_not change(Activity, :count).by(1)
+    @activity.description = ""
+    @activity.save.should be_false
+    @activity.should have(1).error_on(:description)
+    @xbox_identity.activities.should have(:no).record
+    Activity.should have(:no).record
   end
 
   it "requires 180 character limit on description" do
-    lambda {
-      @activity.description = "This is an extremely long description and it exceeds the total number of characters allowed. In order to make this description longer, I have to add more characters. Here are some more characters"
-      @activity.save.should be_false
-      @activity.errors.on(:description).should be_true
-      @xbox_console_user.activities.count.should == 0
-    }.should_not change(Activity, :count).by(1)
+    @activity.description = "This is an extremely long description and it exceeds the total number of characters allowed. In order to make this description longer, I have to add more characters. Here are some more characters"
+    @activity.save.should be_false
+    @activity.errors.on(:description).should be_true
+    @xbox_identity.activities.should have(:no).record
+    Activity.should have(:no).record
   end
 
   it "requires a valid avatar id" do
-    lambda {
-      @activity.avatar_id = nil
-      @activity.save.should be_false
-      @activity.errors.on(:avatar).should be_true
-      @activity.errors.full_messages.include?("Avatar must be associated with an activity").should be_true
-    }.should_not change(Activity, :count).by(1)
+    @activity.avatar_id = nil
+    @activity.save.should be_false
+    @activity.errors.on(:avatar).should be_true
+    @activity.errors.full_messages.should include("Avatar must be associated with an activity")
+    @xbox_identity.activities.should have(:no).record
+    Activity.should have(:no).record
   end
 
   it "requires a valid avatar type" do
-    lambda {
-      @activity.avatar_type = "InvalidType"
-      @activity.save.should be_false
-      @activity.errors.on(:avatar).should be_true
-      @activity.errors.full_messages.include?("Avatar must be associated with an activity").should be_true
-    }.should_not change(Activity, :count).by(1)
+    @activity.avatar_type = "InvalidType"
+    @activity.save.should be_false
+    @activity.should have(1).error_on(:avatar)
+    @activity.errors.full_messages.should include("Avatar must be associated with an activity")
+    Activity.should have(:no).record
   end
 
   it "should get recent activities" do
-    2.times { |n| Factory(:activity, {:description => "This is activity # #{n}", :avatar => @xbox_console_user, :created_at => Time.now - 7.days})}
-    4.times { |n| Factory(:activity, {:description => "This is a recent activity, Number #{n} to be exact.", :avatar => @xbox_console_user})}
+    2.times { |n| Factory(:activity, {:description => "This is activity # #{n}", :avatar => @xbox_identity, :created_at => Time.now - 7.days})}
+    4.times { |n| Factory(:activity, {:description => "This is a recent activity, Number #{n} to be exact.", :avatar => @xbox_identity})}
     Activity.recent.count.should == 4
   end
 
